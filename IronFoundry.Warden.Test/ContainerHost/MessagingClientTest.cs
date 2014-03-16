805b21462c57932a41a66b93e331bf225a6f3e27
Bob Brumfield <bob.brumfield@gmail.com>
2014-03-16 11:13:14 -0700

    Start of strongly typed send and response

--- IronFoundry.Warden.Test/ContainerHost/MessagingClientTest.cs
+++ IronFoundry.Warden.Test/ContainerHost/MessagingClientTest.cs
@@ -14,7 +14,8 @@ namespace IronFoundry.Warden.Test.ContainerHost
     public class MessagingClient
     {
         private Action<string> transportHandler;
-        private Dictionary<string, TaskCompletionSource<JsonRpcResponse>> awaitingResponse = new Dictionary<string, TaskCompletionSource<JsonRpcResponse>>();
+        private Dictionary<string, ResponsePublisher> awaitingResponse =
+            new Dictionary<string, ResponsePublisher>();
 
         public MessagingClient(Action<string> transportHandler)
         {
@@ -23,33 +24,101 @@ namespace IronFoundry.Warden.Test.ContainerHost
 
         public Task<JsonRpcResponse> SendMessageAsync(JsonRpcRequest r)
         {
-            var tcs = new TaskCompletionSource<JsonRpcResponse>();
-            awaitingResponse.Add(r.id, tcs);
-
+            var publisher = new DefaultResponsePublisher();
+            awaitingResponse.Add(r.id, publisher);
             transportHandler(JsonConvert.SerializeObject(r, Formatting.None));
+            return publisher.Task;
+        }
 
-            return tcs.Task;
+        public Task<TResult> SendMessageAsync<T, TResult>(T request)
+            where T: JsonRpcRequest
+            where TResult: JsonRpcResponse
+        {
+            var publisher = new StronglyTypedResponsePublisher<TResult>();
+            awaitingResponse.Add(request.id, publisher);
+            transportHandler(JsonConvert.SerializeObject(request, Formatting.None));
+            return publisher.Task;
         }
 
         public void PublishResponse(JObject response)
         {
             string id = response["id"].ToString();
-            TaskCompletionSource<JsonRpcResponse> tcs;
-            if (awaitingResponse.TryGetValue(id, out tcs))
+            ResponsePublisher publisher;
+            if (awaitingResponse.TryGetValue(id, out publisher))
             {
-                if (response["error"] != null)
-                {
-                    var errorResponse = new JsonRpcErrorResponse(id);
-                    errorResponse.error.Code = (int)response["error"]["code"];
-                    errorResponse.error.Message = response["error"]["message"].ToString();
-                    errorResponse.error.Data = response["error"]["data"] == null ? null : response["error"]["data"].ToString();
+                publisher.Publish(response);
+            }
+        }
 
-                    tcs.SetResult(errorResponse);
+        private abstract class ResponsePublisher
+        {
+            abstract public void Publish(JObject response);
+
+            protected bool IsErrorResponse(JObject response)
+            {
+                return (response["error"] != null);
+            }
+
+            protected JsonRpcErrorResponse BuildErrorResponse(JObject error)
+            {
+                var errorResponse = new JsonRpcErrorResponse(error["id"].ToString());
+                errorResponse.error.Code = (int)error["error"]["code"];
+                errorResponse.error.Message = error["error"]["message"].ToString();
+                errorResponse.error.Data = error["error"]["data"] == null ? null : error["error"]["data"].ToString();
+
+                return errorResponse;
+            }
+        }
+
+        private class DefaultResponsePublisher : ResponsePublisher
+        {
+            TaskCompletionSource<JsonRpcResponse> tcs = new TaskCompletionSource<JsonRpcResponse>();
+
+            public DefaultResponsePublisher()
+            {
+            }
+
+            public override void Publish(JObject arg)
+            {
+                if (IsErrorResponse(arg))
+                {
+                    tcs.SetResult(BuildErrorResponse(arg));
                 }
                 else
                 {
-                    var rpcResponse = new JsonRpcResponse(id);
+                    var rpcResponse = new JsonRpcResponse(arg["id"].ToString());
                     tcs.SetResult(rpcResponse);
+                }
+            }
+
+         public Task<JsonRpcResponse> Task
+            {
+                get
+                {
+                    return tcs.Task;
+                }
+            }
+        }
+
+        private class StronglyTypedResponsePublisher<TResponse> : ResponsePublisher
+            where TResponse : JsonRpcResponse
+        {
+            private TaskCompletionSource<TResponse> tcs = new TaskCompletionSource<TResponse>();
+
+            public StronglyTypedResponsePublisher()
+            {
+            }
+
+            override public void Publish(JObject response)
+            {
+                tcs.SetResult(response.ToObject<TResponse>());
+            }
+
+            public Task<TResponse> Task
+            {
+                get
+                {
+                    return tcs.Task;
                 }
             }
         }
@@ -57,7 +126,6 @@ namespace IronFoundry.Warden.Test.ContainerHost
 
     public class MessagingClientTest
     {
-
         [Fact]
         public void InvokesSenderToSendMessage()
         {
@@ -152,13 +220,38 @@ namespace IronFoundry.Warden.Test.ContainerHost
             Assert.Equal("Error Data", response.error.Data);
         }
 
-        //[Fact]
-        //public async void StronglyTypedRequestsReturnsStronglyTypedResponse()
-        //{
-        //    MessagingClient client = null;
 
+        [Fact]
+        public async void StronglyTypedRequestsReturnsStronglyTypedResponse()
+        {
+            MessagingClient client = null;
+            var r = new CustomRequest();
 
-        //}
+            client = new MessagingClient(m =>
+            {
+                client.PublishResponse(new JObject(
+                    new JProperty("jsonrpc", "2.0"),
+                    new JProperty("id", r.id),
+                    new JProperty("result", "ResultData")
+                    ));
+            });
+
+            CustomResponse response = await client.SendMessageAsync<CustomRequest,CustomResponse>(r);
+            Assert.NotNull(response);
+        }
+
+        class CustomRequest : JsonRpcRequest
+        {
+            public CustomRequest() : base("CustomRequestMethod")
+            {
+
+            }
+        }
+
+        class CustomResponse : JsonRpcResponse<string>
+        {
+
+        }
 
         // Strongly typed handling
         // Uncorrelated Response throws?
